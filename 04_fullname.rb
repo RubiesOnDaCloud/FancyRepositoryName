716c6275574a1673c6f916f357b73eefe2c2eeda
laushinka <laurie.malau@gmail.com>
2015-02-03 20:57:13 +0100

    Create 04_fullname.rb

---
+++ 04_fullname.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what is your first name?'
+first_name = gets.chomp
+puts 'And what is your middle name?'
+middle_name = gets.chomp
+puts 'Finally, what is your last name?'
+last_name = gets.chomp
+puts 'Thank you, ' + first_name + ' ' + middle_name + ' ' + last_name + '! Nice to meet you :)'
